3269ef700a449d557eaaebc1d582afa56ece45cc
Hau Hoang <hauhoangduc@gmail.com>
2022-09-21 11:10:56 -0700

    Initial Commit

---
+++ AgeCalculator/AgeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeCalculatorApp.swift
+//  AgeCalculator
+//
+//  Created by Hau Hoang on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct AgeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
